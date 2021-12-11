0dfab2057f099d747a67a6ba56e3a9278b999c19
rzasinets <roman.zasinets@gmail.com>
2021-12-11 18:22:06 +0300

    refactor: payload-based mutations

--- gql-hotchocolate-skd60/Controllers/Mutation.cs
+++ gql-hotchocolate-skd60/Controllers/Mutation.cs
@@ -4,24 +4,16 @@
 
   using gql_hotchocolate_skd60.Models;
 
-  using HotChocolate.AspNetCore.Authorization;
-
   public class Mutation
   {
-    public Book? CreateBook(CreateBookInput input)
+    public CreateBookPayload? CreateBook(CreateBookInput input)
     {
       if (input.ThrowEr)
       {
         throw new Exception("Ex");
       }
-      return new Book
-        {
-          Title = input.Title,
-          Author = new Author
-            {
-              Name = "Jon Skeet"
-            }
-        };
+
+      return new CreateBookPayload { Book = new Book { Title = input.Title, Author = new Author { Name = "Jon Skeet" } } };
     }
       
   }
--- gql-hotchocolate-skd60/Models/CreateBookInput.cs
+++ gql-hotchocolate-skd60/Models/CreateBookInput.cs
@@ -2,7 +2,9 @@
 {
   public class CreateBookInput
   {
+#pragma warning disable CS8618
     public string Title { get; set; }
+#pragma warning restore CS8618
     public bool ThrowEr { get; set; }
   }
 }
